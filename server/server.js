0b0978c9ed5bf4391293fd258a7266dc898bd9c6
Luis Eduardo <luiseduardo20.dev@gmail.com>
2022-10-20 22:21:22 -0300

    feat: server binding

--- server/server.js
+++ server/server.js
@@ -1,34 +1,45 @@
 // const os = require('os');
-const dgram = require('dgram');
-const net = require('net');
+const dgram = require("dgram");
+const net = require("net");
 
-const server = dgram.createSocket('udp4');
+var PORT = 6024;
+var MULTICAST_ADDR = "239.255.255.250";
+
+const server = dgram.createSocket("udp4");
 
 // var networkInterfaces = os.networkInterfaces();
 
-server.on('error', (err) => {
-    console.log(err);
-    server.close();
+server.on("error", (err) => {
+  console.log(err);
+  server.close();
 });
 
-server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
+server.on("listening", () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-    server.send('Received!', rinfo.port);
+server.on("message", (msg, rinfo) => {
+  console.log(
+    `server got: ${msg} from ${rinfo.address}:${rinfo.port}`
+  );
+  server.send("Received!", rinfo.port);
 });
 
-server.bind(9999, () => {
-    server.setBroadcast(true);
+server.bind(PORT, () => {
+  // server.setBroadcast(true);
+  server.addMembership(MULTICAST_ADDR);
 });
 
-net.createServer((sock) => {
-    console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+net
+  .createServer((sock) => {
+    console.log(
+      "CONNECTED: " + sock.remoteAddress + ":" + sock.remotePort
+    );
 
-    sock.on('data', (data) => {
-        console.log(data);
-    })
-}).listen(9998, '127.0.0.1');
+    sock.on("data", (data) => {
+      console.log(data);
+    });
+  })
+  .listen(9998, "127.0.0.1");
+// .listen(9998, "26.80.196.74");
